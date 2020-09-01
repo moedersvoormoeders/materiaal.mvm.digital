7db053edbb8f88b27496b9465f547fd0462a9da7
Maartje Eyskens <maartje@eyskens.me>
2020-09-01 21:23:38 +0200

    Initial prototype move to mvm.digital
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Search from '../views/Search.vue'
+import Login from '../views/Login.vue'
+import Details from '../views/Details.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/search',
+    name: 'search',
+    component: Search
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/details/:id',
+    name: 'details',
+    component: Details,
+    props: true,
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  // redirect to login page if not logged in and trying to access a restricted page
+  const publicPages = ['/login'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if (authRequired && !loggedIn) {
+    return next('/login');
+  }
+
+  next();
+})
+
+router.push("search")
+
+export default router
